524ccc194c3dbc14465c74de0b8ef19de2a33d8a
1Malevolence1 <danil36rusdanil@ mail.ru>
2023-07-10 22:40:36 +0300

    completed 6-8

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
 
@@ -57,6 +59,35 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+
+    }
+    public static void task3 () {
+        System.out.println("\nЗадача 6");
+        var boxer_1 = 78.2;
+        var boxer_2 = 82.7;
+        var sumBoxers = boxer_2 + boxer_1;
+        System.out.println("Общий вес боксеров - " + sumBoxers);
+        var difBoxers = boxer_2 - boxer_1;
+        System.out.println("Разница между весами боксеров - " + difBoxers);
+
+
+        System.out.println("\nЗадача 7");
+        System.out.println("Первый способ: " + (boxer_2 - boxer_1));
+        System.out.println("Второй способ: " + (boxer_2 % boxer_1));
+
+    }
+    public static void task4 () {
+        System.out.println("\nЗадача 8");
+
+        var hour = 640;
+        var personHour = 8;
+        var countPeople = hour / personHour;
+        System.out.println("Всего работников в компании — " + countPeople + " человек.");
+
+        var allPeople = countPeople + 94;
+        var allHour = allPeople * personHour;
+        System.out.println("Если в компании работает " + allPeople + " человек, то всего " + allHour + " часов работы может быть поделено между сотрудниками!");
+
 
 
 
